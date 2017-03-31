3dee485dbe8001e58a830f857b7845846694f790
Eric Liao <eric855104@gmail.com>
2017-03-31 15:51:11 +0800

    修改StringUtil用來濾除html內容的method在處理null字串時不會噴出exception直接回傳null

--- src/core/util/StringUtil.java
+++ src/core/util/StringUtil.java
@@ -3,6 +3,9 @@ package core.util;
 public class StringUtil {
 
 	public static String filterHtml(String htmlString) {
+		if (htmlString == null) {
+			return null;
+		}
 		return htmlString.replaceAll("\\<[^>]*>","").replaceAll("&.*?;","");
 	}
 
